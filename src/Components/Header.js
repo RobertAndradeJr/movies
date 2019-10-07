36eed4c7cd78ee5902d3a084683dd0ffacf84866
Robert Andrade <srandradejr@gmail.com>
2019-10-07 08:51:53 -0500

    added some styles and begin components

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = (props) => {
+    const {text} = props;
+    return (
+        <header className="header">
+            <h2>{text}</h2>
+        </header>
+    )
+}
+
+export default Header
